87e1dc18ba0f90fd0d2f304cff89e013655c0314
Vaiva Staugaityte <91862082+vaivatuta@users.noreply.github.com>
2025-03-17 19:35:55 +0100

    added all images

--- src/js/random-bg.js
+++ src/js/random-bg.js
@@ -1,8 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const images = [
-      "DSC05087.JPEG",
-      "DSC05213.JPEG",
-    ];
+  const images = [
+    "DSC05087.JPEG",
+    "DSC05213.JPEG",
+    "DSC05224.JPEG",
+    "DSC05225.JPEG",
+    "DSC05226.JPEG",
+    "DSC05310.JPEG",
+    "DSC05332.JPEG",
+    "DSC05421.JPEG",
+    "DSC05423.JPEG",
+    "DSC05450.JPEG",
+    "DSC05457.JPEG",
+    "DSC05482.JPEG",
+    "DSC05502.JPEG",
+    "DSC05538.JPEG",
+    "DSC05637.JPEG",
+    "DSC05650.JPEG",
+    "DSC05751.JPEG",
+    "DSC05761.JPEG",
+    "IMG_8132.JPEG",
+    "IMG_8172.JPEG"
+  ];
   
     // get the last used image from local storage
     let lastImage = localStorage.getItem("lastBackground");
